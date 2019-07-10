82a682a1a14b3304396267b3386eff5aaf3edfe6
Racheal <rechealmatsikoIT@gmail.com>
2019-07-10 17:38:28 +0300

    feature(UI):add new images,add agent.html[Fixes #166627329]

--- Javascript/login.js
+++ Javascript/login.js
@@ -8,12 +8,12 @@ function check(form)
                     window.location.href="property.html";
               
             }
-                else if(form.un.value == "user@gmail.com" && form.pw.value== 12345){
-                window.location.href="#";
+                else if(form.un.value == "agent@gmail.com" && form.pw.value== 12345){
+                window.location.href="agent.html";
                
             }
                 else{
-                    alert("Wrong credentials. For testing: 'User'=> {'email':'user@gmail.com', 'password':'12345'}");
+                    alert("Wrong credentials. For testing: 'User'=> {'email':'user@gmail.com', 'password':'12345'},'Agent'=> {'email':'agent@gmail.com', 'password':'12345'}");
              }
             }
         }

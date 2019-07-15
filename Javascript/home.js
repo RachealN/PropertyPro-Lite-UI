7637f22b12bae6b918c759554039fd2a6b543de0
Racheal <rechealmatsikoIT@gmail.com>
2019-07-16 01:57:16 +0300

    bug(UI): Work on feedback -  makes a search bar wide -  Title and Navigation menu centered -  removed hover on description buttons -Enclose each property with its corresponding width -add footer to each page and bgcolor to the footer -add responsiveness to the home page -remove unneccesary space on the post page and center it -add background color on the search button(indexpage)

--- Javascript/home.js
+++ Javascript/home.js
@@ -1,11 +1,11 @@
 function home() {
-    window.location.href = './Templates/login.html'
+    location.replace('./Templates/login.html')
 }
 
 function login() {
-    window.location.href = './Templates/login.html'
+    location.replace('./Templates/login.html')
 }
 
 function register() {
-    window.location.href = './Templates/register.html'
+    location.replace('./Templates/register.html')
 }
